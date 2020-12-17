40ac3c36dd8261c70ff1cd6e8ec84a31ded1835d
Hesham Osman <heshamosman@Heshams-MacBook-Pro.local>
2020-12-17 10:38:28 +0100

    Remove

--- src/main/java/com/gamary/paymentrequestservice/repository/PaymentRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gamary.paymentrequestservice.repository;
-
-import com.gamary.paymentrequestservice.entity.PaymentRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRequestRepository extends JpaRepository<PaymentRequest,Long> {
-}
